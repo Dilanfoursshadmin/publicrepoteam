e5502cdcd203930788222a091210541d01de9709
Sanjaya Dinushantha <sanjayadinushantha@gmail.com>
2019-12-13 10:41:48 +0530

    new change

--- RocketSystem/Controllers/HomeController.cs
+++ RocketSystem/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace RocketSystem.Controllers
         public ActionResult About()
         {
             int a = 0;
-            int y = 0;
+           
             ViewBag.Message = "Your application description page.";
 
             return View();
